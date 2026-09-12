8ae4a3103e0a3c72cbac9da1fe0443813ae4601e
Phyo Han <phyohan@hotmail.com>
2026-09-12 17:12:02 +0800

    Adds names and Matric Number

--- assignment-01/2_hierarchical_solution.py
+++ assignment-01/2_hierarchical_solution.py
@@ -1,21 +1,24 @@
 """Assignment 1 - Problem 2 hierarchical-planning submission template.
 
 * Group Member 1:
-    - Name:
-    - Matric number:
+    - Name: Zhang Jiazheng 
+    - Matric number:A0314707H
 
 * Group Member 2:
-    - Name:
-    - Matric number:
+    - Name: Phyo Han 
+    - Matric number:A0196680R
 
 * Group Member 3:
-    - Name:
-    - Matric number:
+    - Name: Wang Shiyu 
+    - Matric number:A0354696L
 
 * Group Member 4:
-    - Name:
-    - Matric number:
-"""
+    - Name: Liu Hengyan 
+    - Matric number:A0350634J
+
+* Group Member 5:
+    - Name: Cui Yi 
+    - Matric number:A0353244J
 
 from __future__ import annotations
 
